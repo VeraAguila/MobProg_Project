6248896f1d2f49d6b7362d081bf12cf807c394fe
Akane728 <akaneseijisan@gmail.com>
2025-11-23 04:47:03 +0800

    sign up screen

--- PusoMalaya/puso_malaya/lib/login/signup_screen.dart
+++ PusoMalaya/puso_malaya/lib/login/signup_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:puso_malaya/login/login_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -11,7 +14,213 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/signup_bg.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.3),
+            ),
+          ),
+
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 60),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      SizedBox(width: 10),
+                      Text(
+                        "Puso Malaya",
+                        style:
+                            Theme.of(
+                              context,
+                            ).textTheme.titleLarge!.copyWith(
+                              color: Colors.white,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 380),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
+                      child: Container(
+                        padding: EdgeInsets.all(25),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(
+                            color: Colors.white.withOpacity(0.3),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Sign Up",
+                              style:
+                                  Theme.of(
+                                    context,
+                                  ).textTheme.titleMedium!.copyWith(
+                                    color: Colors.white,
+                                  ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Create account to continue.",
+                              style:
+                                  Theme.of(
+                                    context,
+                                  ).textTheme.titleSmall!.copyWith(
+                                    color: Colors.white,
+                                  ),
+                            ),
+
+                            SizedBox(height: 25),
+
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.25),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: TextField(
+                                style: Theme.of(context).textTheme.titleSmall!
+                                    .copyWith(color: Colors.white),
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(
+                                    Icons.person,
+                                    color: Colors.white.withOpacity(0.8),
+                                  ),
+                                  hintText: "Email",
+                                  hintStyle: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .copyWith(
+                                        color: Colors.white.withOpacity(
+                                          0.7,
+                                        ),
+                                      ),
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(height: 15),
+
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.25),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: TextField(
+                                obscureText: true,
+                                style: Theme.of(context).textTheme.titleSmall!
+                                    .copyWith(color: Colors.white),
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.white.withOpacity(0.8),
+                                  ),
+                                  hintText: "Password",
+                                  hintStyle: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .copyWith(
+                                        color: Colors.white.withOpacity(
+                                          0.7,
+                                        ),
+                                      ),
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(height: 10),
+
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 12,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      15,
+                                    ),
+                                  ),
+                                ),
+                                child: Text("Login"),
+                              ),
+                            ),
+
+                            SizedBox(height: 10),
+
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Already have an account? Go to ",
+                                  style: Theme.of(context).textTheme.titleSmall!
+                                      .copyWith(color: Colors.white70),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginScreen(),
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    "Login",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleSmall!
+                                        .copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
